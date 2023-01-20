34f098eb0d6e574184a5fa92ee160192cb108cb7
mac <ideer@yandex.ru>
2023-01-21 00:43:01 +0500

    1.6. Взаимодействие объектов [#166087]

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public void tryRun(boolean condition) {
+        if (condition) {
+            System.out.println("Ball die");
+        } else {
+            System.out.println("Ball run away");
+        }
+    }
+}
