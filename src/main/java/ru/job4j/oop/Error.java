34128911c6913e70b0d41ae8ef9ccdc257928177
mac <ideer@yandex.ru>
2023-01-23 19:03:07 +0500

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,30 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error(boolean active,int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+    public Error() {
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error(true, 6, "Error 404");
+        Error errors = new Error();
+        error.printInfo();
+        errors.printInfo();
+    }
+
+}
