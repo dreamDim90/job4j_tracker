f2359af95ca0d1b405079ad260a59e5a2c4c805b
dreamdim <ideer@yandex.ru>
2023-02-19 22:48:25 +0500

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,15 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFIO("Nazukin Vldislav Andreevich");
+        student.setGroup("AD 07");
+        student.setCreated(new Date());
+        System.out.println(student.getFIO());
+        System.out.println(student.getGroup());
+        System.out.println(student.getCreated());
+    }
+}
