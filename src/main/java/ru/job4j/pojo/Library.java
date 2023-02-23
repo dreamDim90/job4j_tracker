d34124186789e907c5da8fc2b01999ae92af977e
dreamdim <ideer@yandex.ru>
2023-02-23 18:07:37 +0500

    3. Массивы и модели. [#174892]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -26,7 +26,7 @@ public class Library {
         System.out.println("Shown only book Clean code");
         for (int index = 0; index < books.length; index++) {
             Book bo = books[index];
-            if (bo.getName().equals("Clean code")) {
+            if ("Clean code".equals(bo.getName())) {
                 System.out.println(bo.getName() + " - " + bo.getPages());
             }
         }
