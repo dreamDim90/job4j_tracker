5f95030f6b37cb4a16d2a48dda59bf7d97d5106c
dreamdim <ideer@yandex.ru>
2023-02-28 22:37:03 +0500

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -31,18 +31,7 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        /*Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;*/
-        /* Находим индекс */
         int index = indexOf(id);
-        /* Если индекс найден возвращаем item, иначе null */
         return index != -1 ? items[index] : null;
     }
 
@@ -59,6 +48,11 @@ public class Tracker {
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        item.setId(id) = ;
+            if (index != -1) {
+                item.setId(id);
+                items[index] = item;
+                return true;
+            }
+            return false;
     }
 }
