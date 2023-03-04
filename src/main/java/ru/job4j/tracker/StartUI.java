83a4e6d38a4aaed90158e1df46b9b1f7af74a17c
dreamdim <ideer@yandex.ru>
2023-03-04 06:42:15 +0500

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,8 +6,10 @@ public class StartUI {
 
     public static void main(String[] args) {
         Item item = new Item();
+        Item itemTest = new Item();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
         String currentDateTimeFormat = formatter.format(item.getCreated());
         System.out.println(currentDateTimeFormat);
+        System.out.println(itemTest);
     }
 }
