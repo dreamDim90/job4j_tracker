bd9c87628e421b5f5047b573b40a0faef4dbafaa
dreamdim <ideer@yandex.ru>
2023-03-12 22:23:30 +0500

    4.6.2. Switch - matcher [#334071]

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -9,10 +9,10 @@ public class MagicBall {
         System.out.print("Я великий Оракул. Что ты хочешь узнать? ");
         String qestion = input.nextLine();
         int answer = new Random().nextInt(3);
-            switch (answer) {
-                case 0 -> System.out.println("Да");
-                case 1 -> System.out.println("Нет");
-                default -> System.out.println("Может быть");
-            };
+        switch (answer) {
+            case 0 -> System.out.println("Да");
+            case 1 -> System.out.println("Нет");
+            default -> System.out.println("Может быть");
+        }
     }
 }
