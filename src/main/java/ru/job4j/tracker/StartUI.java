7f278f31d60ff35a364d6168d00248c9af374f70
dreamdim <ideer@yandex.ru>
2023-03-22 22:43:43 +0500

    3.1. Реализация класса StartUI. Добавление заявки [#500744]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -11,7 +11,7 @@ public class StartUI {
             System.out.print("Select: ");
             int select = Integer.parseInt(scanner.nextLine());
             if (select == 0) {
-                System.out.println("=== Create a new Item ===");
+                System.out.println("== Create a new Item ==");
                 System.out.print("Enter name: ");
                 String name = scanner.nextLine();
                 Item item = new Item(name);
