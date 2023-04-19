f523b081d117b2d0059fc2e61e2bf5304c426272
dreamdim <ideer@yandex.ru>
2023-04-19 21:46:59 +0500

    11.0. Шаблон проектирования - Стратегия [#786]

---
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -0,0 +1,10 @@
+package ru.job4j.strategy;
+
+public class Square implements Shape {
+    public String draw() {
+        String ln = System.lineSeparator();
+        return    "|------| " + ln
+                + "|      | " + ln
+                + "|______| " + ln;
+    }
+}
