8baa104b877cbcd69605b0071c1703e1f7b4c9fc
dreamdim <ideer@yandex.ru>
2023-05-02 21:31:54 +0500

    12.1 Зависимость от System.out [#33568]

--- src/main/java/ru/job4j/tracker/StubOutput.java
+++ src/main/java/ru/job4j/tracker/StubOutput.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker;
 
 public class StubOutput implements Output {
     private final StringBuilder buffer = new StringBuilder();
+
     @Override
     public void println(Object obj) {
         if (obj != null) {
