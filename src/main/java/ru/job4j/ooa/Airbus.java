297d229daa4843c942e844f4b19538be21aaba1b
dreamdim <ideer@yandex.ru>
2023-08-28 23:14:26 +0500

    3. final [#334073 # [#334073]]

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -3,6 +3,7 @@ package ru.job4j.ooa;
 public final class Airbus extends Aircraft {
     private static int COUNT_ENGINE = 2;
     public String name;
+
     public Airbus(String name) {
         this.name = name;
     }
@@ -14,15 +15,16 @@ public final class Airbus extends Aircraft {
     public String getName() {
         return name;
     }
+
     @Override
     public void printModel() {
         System.out.println("Модель самолета: " + name);
     }
+
     public void printCountEngine() {
         String a380 = "A380";
         COUNT_ENGINE = name.equals(a380) ? 4 : 2;
         System.out.println("Количество двигателей равно: " + COUNT_ENGINE);
-
     }
 
     @Override
