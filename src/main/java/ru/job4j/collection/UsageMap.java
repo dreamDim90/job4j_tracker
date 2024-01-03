4964c25f31958165b5ab83832e21030542e6d907
dreamdim <ideer@yandex.ru>
2024-01-03 19:38:19 +0500

    0. Map, HashMap [#212652 #470104]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,10 @@ public class UsageMap {
     public static void main(String[] args) {
     HashMap<String, String> map = new HashMap<>();
     map.put("idim@yandex.ru", "Dmitriy Mihaylin");
+    map.put("alex@yandex.ru", "Alexey Alexeev");
+    map.put("rom77@yandex.ru", "Rovan Romanov");
+    map.put("ilex@yandex.ru", "Lex Luter");
+    map.put("idim@yandex.ru", "Peter Parker");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " - " + value);
