d3bca3e4a62023b1f0814700f7995ce74cadce36
dreamdim <ideer@yandex.ru>
2024-01-09 21:52:45 +0500

    4. Аттестация [#504878]

---
+++ src/main/java/ru/job4j/hashmap/AnalyzeByMap.java
@@ -0,0 +1,73 @@
+package ru.job4j.hashmap;
+
+import java.util.*;
+
+import static java.util.Comparator.naturalOrder;
+
+public class AnalyzeByMap {
+    public static double averageScore(List<Pupil> pupils) {
+        double score = 0;
+        int subjects = 0;
+        for (Pupil pupil : pupils) {
+            for (Subject subject : pupil.subjects()) {
+                 score += subject.score();
+                 subjects ++;
+            }
+        }
+        return score / subjects;
+    }
+
+    public static List<Label> averageScoreByPupil(List<Pupil> pupils) {
+        List<Label> rsl = new ArrayList<>();
+        for (Pupil pupil : pupils) {
+            double sumScore = 0;
+            for (Subject subject : pupil.subjects()) {
+                sumScore += subject.score();
+            }
+            rsl.add(new Label(pupil.name(), sumScore / pupil.subjects().size()));
+        }
+        return rsl;
+    }
+
+    public static List<Label> averageScoreBySubject(List<Pupil> pupils) {
+        Map<String, Integer> values = new HashMap<>();
+        List<Label> rsl = new ArrayList<>();
+        for (Pupil pupil : pupils) {
+            for (Subject subject : pupil.subjects()) {
+                values.put(subject.name(), values.getOrDefault(subject.name(), 0) + subject.score());
+            }
+        }
+        for (String value : values.keySet()) {
+            rsl.add(new Label(value, (double) values.get(value) / pupils.size()));
+        }
+         return rsl;
+    }
+
+    public static Label bestStudent(List<Pupil> pupils) {
+        List<Label> rsl = new ArrayList<>();
+        for (Pupil pupil : pupils) {
+            double sumScore = 0;
+            for (Subject subject : pupil.subjects()) {
+                sumScore += subject.score();
+            }
+            rsl.add(new Label(pupil.name(), sumScore));
+            rsl.sort(Comparator.naturalOrder());
+        }
+        return rsl.get(rsl.size() - 1);
+    }
+
+    public static Label bestSubject(List<Pupil> pupils) {
+        Map<String, Integer> values = new HashMap<>();
+        List<Label> rsl = new ArrayList<>();
+        for (Pupil pupil : pupils) {
+            for (Subject subject : pupil.subjects()) {
+                values.put(subject.name(), values.getOrDefault(subject.name(), 0) + subject.score());
+            }
+        }
+        for (String value : values.keySet()) {
+            rsl.add(new Label(value, values.get(value)));
+        }
+        rsl.sort(Comparator.naturalOrder());
+        return rsl.get(rsl.size() - 1);
+    }
+}
