f3f2c24c3336f8fe81263385efa3877603ee1e21
dreamdim <ideer@yandex.ru>
2024-01-09 21:58:16 +0500

    4. Аттестация [#504878]

--- src/main/java/ru/job4j/hashmap/AnalyzeByMap.java
+++ src/main/java/ru/job4j/hashmap/AnalyzeByMap.java
@@ -11,7 +11,7 @@ public class AnalyzeByMap {
         for (Pupil pupil : pupils) {
             for (Subject subject : pupil.subjects()) {
                  score += subject.score();
-                 subjects ++;
+                 subjects++;
             }
         }
         return score / subjects;
