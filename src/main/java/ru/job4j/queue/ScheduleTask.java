d14e8f3435956770e308c5e8559d08f613c88f6c
dreamdim <ideer@yandex.ru>
2024-01-22 15:58:01 +0500

    5. PriorityQueue [#504944]

---
+++ src/main/java/ru/job4j/queue/ScheduleTask.java
@@ -0,0 +1,26 @@
+package ru.job4j.queue;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class ScheduleTask {
+    private PriorityQueue<Task> queue;
+
+    public ScheduleTask(Comparator<Task> comparator) {
+        this.queue = new PriorityQueue<>(comparator);
+    }
+
+    public void addTask(Task task) {
+       queue.offer(task);
+    }
+
+    public Task readTask() {
+        return queue.peek();
+    }
+
+    public Task getTask() {
+        Task el = queue.peek();
+        queue.poll();
+        return el;
+    }
+}
